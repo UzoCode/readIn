48a31629c8fb439cc46467971823a2e3981bcf7a
UzoCode <akosabenedict@gmail.com>
2025-01-22 17:01:40 +0100

    Integrate service and controller file for authentication

--- readin-backend/src/auth/auth.module.ts
+++ readin-backend/src/auth/auth.module.ts
@@ -2,11 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaService } from '../prisma.service';
-import { MailService } from '../mail/mail.service'; // Import path as necessary could adjusted 
-
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService, PrismaService, MailService],
+  providers: [AuthService, PrismaService],
 })
 export class AuthModule {}
