5ed3a044b6365babede24b1187f1f47f2bf7a551
UzoCode <akosabenedict@gmail.com>
2025-01-22 16:43:34 +0100

    Updated properties to keep them optional

--- readin-backend/src/auth/dto/reset-password.dto.ts
+++ readin-backend/src/auth/dto/reset-password.dto.ts
@@ -1,9 +1,12 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
+  @IsNotEmpty()
   @IsString()
-  token?: string; // This property is Optional
+  token?: string; // This could be a token sent through another method
 
+  @IsNotEmpty()
   @IsString()
-  newPassword?: string; // This property is Optional
+  @MinLength(6) // Minimum length for the new password
+  newPassword?: string;
 }
